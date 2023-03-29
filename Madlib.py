a716e8ad6f23c5ba5977dab6bcb5af48a11a8a06
Tanmay Khunger <117931543+tanmaykhunger@users.noreply.github.com>
2023-03-29 15:54:21 +0530

    The first commit

---
+++ Madlib.py
@@ -0,0 +1,5 @@
+adjective = input("Please enter an adjective: ")
+noun = input("Please enter a noun: ")
+verb = input("Please enter a verb: ")
+print("Your Madlib:")
+print("The", adjective, noun, verb, "over the lazy brown dog.")
